8eb8d87bd83a2a3b0bbd324d8cca6df7acce3998
waqas4334 <56790171+waqas4334@users.noreply.github.com>
2022-11-03 10:43:23 +0500

    first commit

---
+++ src/navigation/navigationStrings.js
@@ -0,0 +1,8 @@
+export default {
+    Login:"login",
+    Signup:"signup",
+    Example:"exp",
+    Home:"home",
+    drawerMainPage:"App",
+    SettingsPage:"setting",
+}
---
+++ src/screens/ChooseAccount/ChooseAccount.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native"
+import React from "react"
+
+const ChooseAccount = () => {
+  return (
+    <View>
+      <Text>ChooseAccount</Text>
+    </View>
+  )
+}
+
+export default ChooseAccount
+
+const styles = StyleSheet.create({})
